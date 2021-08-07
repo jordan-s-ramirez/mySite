1b21c3d7daf3d0b98e57fec18797dbd20cae0eff
jordan-s-ramirez <jordan.s.ramirez.com>
2021-08-07 13:17:44 -0400

    first commit

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -6,16 +6,13 @@ import { stripTags, domHtml } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 
 const pageText = {
-  paraOne: `Hello !! My name is Rolwin Reevan Monteiro. I'm a full stack web developer who is
-    passionate about various web technologies. I like to experiment with different web
-    technologies. I have an experience of 3+ years working with LAMP stack, MERN stack
-    and ELK stack. Building fancy UI's just like this one that your seeing 😅 and writing blogs about tech stacks
-    is what Rolwin loves to do. Check my blog which I update every week for some Javascript and some
-    cool notes on web technologies.`,
-  paraTwo: `Currently I work mostly with Javascript technologies like ReactJS and NodeJS. I also
-    have hands on experience working with cloud infrastructures like <b>AWS/GCP</b> and have deployed applications
-    keeping scalability in mind. Docker, Kubernetes, Jenkins, SonarQube are some of the cool
-    tools I use for <b>CI/ CD</b>. I'm always a learner and a self taught programmer.`,
+  paraOne: `Hello !! My name is Jordan Ramirez... MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. 
+  MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. 
+  MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. 
+  MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. `,
+  paraTwo: `Currently I work... MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. 
+  MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. 
+  MORE COMING SOON. MORE COMING SOON. MORE COMING SOON. I'm always a learner and a self taught programmer.`,
 };
 
 const AboutMe = () => {
@@ -27,7 +24,7 @@ const AboutMe = () => {
           title="About"
           description={description}
           path=""
-          keywords={['Rolwin', 'Reevan', 'Monteiro', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby']}
+          keywords={['Jordan', 'Ramirez', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby']}
         />
         <h1 className="titleSeparate">About Me</h1>
         <p>
@@ -42,15 +39,7 @@ const AboutMe = () => {
             height={60}
             alt="location image"
             textH4="Born and bought up in"
-            textH3="Mangalore, KA, India"
-          />
-        </Col>
-        <Col xs={24} sm={24} md={12} lg={8}>
-          <AboutTile
-            img="coffee.png"
-            alt="coffee image"
-            textH4="Love Coffee"
-            textH3="Coffee + Me = Happiness"
+            textH3="New York, NY"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
@@ -59,14 +48,6 @@ const AboutMe = () => {
             alt="meeting image"
             textH4="Socially Awkward"
             textH3="At times"
-          />
-        </Col>
-        <Col xs={24} sm={24} md={12} lg={8}>
-          <AboutTile
-            img="motorcycle.png"
-            alt="motorcycle image"
-            textH4="Love Riding"
-            textH3="Biker for life"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
@@ -83,8 +64,8 @@ const AboutMe = () => {
           <AboutTile
             img="graduation.png"
             alt="graduation image"
-            textH4="Pursued B.Tech in"
-            textH3="Computer Science"
+            textH4="Pursuing B. Science in"
+            textH3="Computer Engineering"
             height={60}
             width={60}
           />
--- src/components/PageLayout/Sidebar/index.jsx
+++ src/components/PageLayout/Sidebar/index.jsx
@@ -21,12 +21,12 @@ const DomContent = () => (
     <div className={`${style.name} centerAlign`}>
       <div className={`${style.boxName} centerAlign`}>
         <h2>
-          Rolwin
+          Jordan
           {' '}
-          <span>Reevan</span>
+          <span>Ramirez</span>
         </h2>
       </div>
-      <div className={`${style.badge} ${style.badgeGray}`}>Software Engineer</div>
+      <div className={`${style.badge} ${style.badgeGray}`}>Computer Engineer</div>
       <div className="centerAlign box">
         <a href={facebook} target="_blank" label="button" rel="noopener noreferrer"><FA name="facebook-f" /></a>
         <a href={twitter} target="_blank" label="button" rel="noopener noreferrer"><FA name="twitter" /></a>
@@ -39,19 +39,19 @@ const DomContent = () => (
             <FeatherIcon size="19" icon="calendar" />
             {' '}
           </span>
-&nbsp; &nbsp; May 9,1995
+&nbsp; &nbsp; Age: 21
         </li>
         <li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="map-pin" /></span>
           {' '}
-&nbsp; &nbsp; Bangalore, India
+&nbsp; &nbsp; NY, USA
         </li>
         <li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="mail" /></span>
           {' '}
 &nbsp; &nbsp;
           <a
-            href="mailto:&#114;&#111;&#108;&#119;&#105;&#110;&#109;&#111;&#110;&#116;&#101;&#105;&#114;&#111;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
+            href="mailto:jordan.s.ramirez@gmail.com"
             target="_top"
           >
             <span className={style.emailHider}>@</span>
