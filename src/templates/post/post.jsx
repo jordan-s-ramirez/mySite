2b1d39e4b546e7fde8ea94cc8ba9b557a3f15ba1
jordan-s-ramirez <jordan.s.ramirez.com>
2021-08-08 15:15:40 -0400

    blog update

--- src/templates/post/post.jsx
+++ src/templates/post/post.jsx
@@ -31,7 +31,7 @@ const Post = ({ data }) => {
           title={title}
           description={excerpt}
           path={path}
-          keywords={['Rolwin', 'Reevan', 'Monteiro', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby', 'technology']}
+          keywords={['Jordan', 'Ramirez', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby', 'technology']}
         />
         <Header />
         <SidebarWrapper>
